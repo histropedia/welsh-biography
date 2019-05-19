918f3695e49d05206542cdb8e4fca1a4a094bc0a
Navino Evans <navino@histropedia.com>
2019-05-20 00:31:08 +0100

    Add getOrderedColorGroups function
    An ordered version of the colour code groups are needed in order to assign colours and render the legend.
    - AppcolorGroupsOrdered is populated by the new function during App.setColorCode
    - Array is ordred by visibleCount, or by id if count is the same (to prevent random re-ordering)
    - Colours in App.options are assigned in sequence according to order of color code groups
    - Colours are set in both colorCodeGroups and colorCodeGroupsOrdered at once

--- js/color-code.js
+++ js/color-code.js
@@ -1,26 +1,28 @@
 //  Extends App class with all colour code related properties and methods
 //  Includes private functions and variables, and click handlers for user changing the colour code
 //
-//  App.colorCodeGroups stores all data for the active color code, populated by getColorCodeGroups(property)
+//  App.colorGroups stores all data for the active color code, populated by getcolorGroups(property)
 //      - Each group uses Wikidata Q number as key (e.g. "Q49757" for a poet)
 //      - Contains group label in en and cy
 //      - visibleCount to show how many articles currently match
 //      - complete list of actual articles objects on the timeline
 //
-//  App.colorCodeGroups is then used for all further actions, like applying article colours,
+//  App.colorGroups is then used for all further actions, like applying article colours,
 //  rendering the legend or filtering the timeline
 
 (function() {
 
-    App.prototype.colorCodeGroups = {} // groups for currently applied colour code
+    App.prototype.colorGroups = {}          // groups for currently applied colour code
+    App.prototype.colorGroupsOrdered = []   // used for assigning colours, and rendering the legend
     
     App.prototype.setupColorCodeOptions = function() {
         // add dropdown colour code options to UI
     }
     
     App.prototype.setColorCode = function(property) {
-        this.colorCodeGroups = getColorCodeGroups(this.timeline, property);
-        
+        this.colorGroups = getcolorGroups(this.timeline, property);
+        this.orderedColorGroups = getOrderedColorGroups(this.colorGroups);
+        console.log(this.orderedColorGroups);
         // renderLegend()
         
         this.state.appliedColorCode = property;
@@ -28,7 +30,7 @@
     
     /****************** Private functions ******************/
     
-    function getColorCodeGroups(timeline, property) {
+    function getcolorGroups(timeline, property) {
         // Loop through articles, store each Q number found as key in colour groups object
         // Articles without a statement using selected property are stored with "noValue" key instead
         var colorGroups = {};
@@ -57,6 +59,24 @@
         }
         
         return colorGroups;
+    }
+        
+    function getOrderedColorGroups(colorGroups) {
+        var sorter = function(a, b) {
+            var result = b.visibleCount - a.visibleCount;
+            if (result === 0) result = b.item > a.item;
+            return result;
+        };
+        
+        var orderedGroups = Object.entries(colorGroups).map(function(entry) {
+            //combine the two array elements of each entry returned by Object.entries()
+            var item = entry[0];
+            var cleanedEntry = entry[1];
+            entry[1].item = item;
+            return entry[1];
+        });
+        
+        return orderedGroups.sort(sorter);
     }
     
 })() 
