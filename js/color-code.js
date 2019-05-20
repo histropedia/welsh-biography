5b26182e4e2e2eb25728f1c3242f60b772bfee45
Navino Evans <navino@histropedia.com>
2019-05-20 09:53:39 +0100

    Add assignColorGroupColors private function
    - Assigns colours from list provided in App.options.colorCode.colors
    - Uses App.orderedColorGroups to assign the colors, with the first color from the options being assigned to the group with highest count of visible articles on the timeline
    - Also stores in App.colourGroups object for easy access when colouring the timeline articles

--- js/color-code.js
+++ js/color-code.js
@@ -13,7 +13,7 @@
 (function() {
 
     App.prototype.colorGroups = {}          // groups for currently applied colour code
-    App.prototype.colorGroupsOrdered = []   // used for assigning colours, and rendering the legend
+    App.prototype.orderedColorGroups = []   // used for assigning colours, and rendering the legend
     
     App.prototype.setupColorCodeOptions = function() {
         // add dropdown colour code options to UI
@@ -22,7 +22,9 @@
     App.prototype.setColorCode = function(property) {
         this.colorGroups = getcolorGroups(this.timeline, property);
         this.orderedColorGroups = getOrderedColorGroups(this.colorGroups);
+        assignColorGroupColors(this);
         console.log(this.orderedColorGroups);
+        console.log(this.colorGroups);
         // renderLegend()
         
         this.state.appliedColorCode = property;
@@ -30,9 +32,8 @@
     
     /****************** Private functions ******************/
     
+    // Get main colorGroups object used by all other colour code related functions
     function getcolorGroups(timeline, property) {
-        // Loop through articles, store each Q number found as key in colour groups object
-        // Articles without a statement using selected property are stored with "noValue" key instead
         var colorGroups = {};
         var articles = timeline.articles;
         for (var i=0; i < articles.length; i++ ) {
@@ -40,9 +41,9 @@
             var statement = article.data.statements[property];
             var statementValue = (statement && statement.values[0]) ?
                 statement.values[0]: 
-                "noValue";
+                "noValue"; // articles without a statement using selected property are stored with "noValue" key instead
             
-            //add new empty colorGroups entry if not already found
+            // add new empty colorGroups entry if not already found
             if (!colorGroups.hasOwnProperty(statementValue)) {
                 colorGroups[statementValue] = {
                     color: "",
@@ -53,14 +54,15 @@
                 };
             }
             
-            //update colorGroups entry
+            // update colorGroups entry
             colorGroups[statementValue].articles.push(article);
             if (!article.isHiddenByFilter) colorGroups[statementValue].visibleCount += 1;
         }
         
         return colorGroups;
     }
-        
+    
+    // Convert colorGroups to array sorted by count of visible articles
     function getOrderedColorGroups(colorGroups) {
         var sorter = function(a, b) {
             var result = b.visibleCount - a.visibleCount;
@@ -69,7 +71,7 @@
         };
         
         var orderedGroups = Object.entries(colorGroups).map(function(entry) {
-            //combine the two array elements of each entry returned by Object.entries()
+            // Combine the two array elements of each entry returned by Object.entries()
             var item = entry[0];
             var cleanedEntry = entry[1];
             entry[1].item = item;
@@ -77,6 +79,18 @@
         });
         
         return orderedGroups.sort(sorter);
+    }
+    
+    // Set the colour used for each colourGroup from list in options
+    function assignColorGroupColors(app) {
+        var colorGroups = app.colorGroups;
+        var orderedColorGroups = app.orderedColorGroups;
+        var colors = app.options.colorCode.colors;
+        for (var i=0; i < orderedColorGroups.length; i++) {
+            var group = orderedColorGroups[i];
+            var item = group.item;
+            group.color = colorGroups[item].color = colors[ i % colors.length - 1]
+        }
     }
     
 })() 
--- js/options.js
+++ js/options.js
@@ -54,6 +54,14 @@ var APP_OPTIONS = {
         P21: {enLabel: "gender", cyLabel: "rhyw"}, 
         P106: {enLabel: "gender", cyLabel: "galwedigaeth"}
     },
-    colorCodes: [],  //list of Wikidata properties
+    colorCode: {
+        properties: [],    // list of Wikidata properties
+        colors: [       // default list of colours to use
+        '#0c0', '#c00', '#00c', '#0cc', '#c0c', '#F1C4D7', '#F199B1', '#EA6B85', '#E14F67', '#00B1EC', '#007DC8', '#00A386',
+        '#ddaa00', '#6677ff', '#333333', '#33ffee', '#33bbff', '#8888ff', '#ffaaee', '#553344', '#994488', '#dd66bb', '#eeaadd',
+        '#eebbdd', '#ddccdd', '#bbaaaa', '#aa88aa', '#886688', '#776677', '#665566', '#443344', '#661155', '#bb3399', '#ee44cc', '#eeffcc', '#ddffbb',
+        '#ccffbb', '#55ff66', '#44ff22', '#00ee77', '#550011', '#990099', '#dd00ff', '#dd66ff'
+        ]
+    }
     
 }
