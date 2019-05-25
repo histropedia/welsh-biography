3510fa6bbca6d5187853d89712d530de928f1a2b
Navino Evans <navino@histropedia.com>
2019-05-25 18:32:26 +0100

    Update options to use labels instead of property/value IDs
    Just for ease of use during development until the multlingual functionality has been added

--- js/options.js
+++ js/options.js
@@ -55,13 +55,8 @@ var APP_OPTIONS = {
         P106: {enLabel: "gender", cyLabel: "galwedigaeth"}
     },
     colorCode: {
-        properties: [],    // list of Wikidata properties
-        colors: [       // default list of colours to use
-        '#0c0', '#c00', '#00c', '#0cc', '#c0c', '#F1C4D7', '#F199B1', '#EA6B85', '#E14F67', '#00B1EC', '#007DC8', '#00A386',
-        '#ddaa00', '#6677ff', '#333333', '#33ffee', '#33bbff', '#8888ff', '#ffaaee', '#553344', '#994488', '#dd66bb', '#eeaadd',
-        '#eebbdd', '#ddccdd', '#bbaaaa', '#aa88aa', '#886688', '#776677', '#665566', '#443344', '#661155', '#bb3399', '#ee44cc', '#eeffcc', '#ddffbb',
-        '#ccffbb', '#55ff66', '#44ff22', '#00ee77', '#550011', '#990099', '#dd00ff', '#dd66ff'
-        ]
+        properties: ["occupation", "gender"],    // list of Wikidata properties
+        colors: ['#961111', '#1a5d1f', '#1565c0', '#7b1fa2', '#b78617', '#d66f6f', '#56ab6c', '#377f9a']
     }
     
 }
