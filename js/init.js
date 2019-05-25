637cfdd6f887b6e4f20c7f2ac38253079c7ca2f2
Navino Evans <navino@histropedia.com>
2019-05-25 18:52:07 +0100

    Add color code setup to initialisation routine

--- js/init.js
+++ js/init.js
@@ -4,8 +4,11 @@ var DWB = new App();
 DWB.setupFilterOptions();
 
 // Create the timeline
-DWB.createTimeline(document.getElementById("timeline-container"), PEOPLE_DATA)
+DWB.createTimeline(document.getElementById("timeline-container"), PEOPLE_DATA);
 
+// Setup colour code
+DWB.setupColorCodeOptions();
+DWB.setColorCode(DWB.options.colorCode.properties[0]);
 
 // Add click events
 $('#btn-open-color-code').click( function() {
@@ -15,3 +18,8 @@ $('#btn-open-color-code').click( function() {
 $('#btn-close-color-code-desktop').click( function() {
     DWB.closeColorCodePanel();
 });
+
+$('#inputGroupSelect01').on('change', function() { //todo: change id
+    var selection = $(this).val();
+    DWB.setColorCode(selection)
+})
