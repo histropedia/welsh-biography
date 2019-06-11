163dc0bb9a08bf08e56fbb48047c16f373a941b5
Navino Evans <navino@histropedia.com>
2019-06-11 12:52:53 +0100

    Add nofx mode activated by url parameter
    - Append ?nofx=true to disable effects and set autostacking range to 'screen'

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 function App() {    
-    var windowWidth = $(window).width();
-    var windowHeight = $(window).height();
+    var windowWidth = $(window).width(),
+        windowHeight = $(window).height(),
+        urlParamters = getUrlParameters(),
+        disableEffects = urlParamters.nofx;
     
     this.isMobile = windowWidth < 450 || windowHeight < 450;
     this.isPortrait = windowWidth < windowHeight;
+    
     
     this.state = {
         // can be used to store states for browser history, or to create share URLs with current state
@@ -28,7 +31,7 @@ function App() {
     this.createTimeline = function(container, articles) {
         var defaultOptions = TIMELINE_OPTIONS.default,
             sizeSpecificOptions = getTimelineOptions(),
-            options = $.extend( true, {}, defaultOptions, sizeSpecificOptions);
+            options = $.extend(true, {}, defaultOptions, sizeSpecificOptions);
         
         this.timeline = new Histropedia.Timeline( container, options );
         this.timeline.load(articles);
@@ -44,19 +47,52 @@ function App() {
     
     /****************** Private functions ******************/
     
-    function updateWindowSettings() {
-        windowWidth = $(window).width();
-        windowHeight = $(window).height();
-        this.isPortrait = windowWidth < windowHeight;
-    }
-    
     function getTimelineOptions() {
         var size = ( windowHeight < 450 ) ? 'small' : 'large';
         //deep copy of size specific options
         var options = $.extend( true , {}, TIMELINE_OPTIONS[size] );
         options.width = windowWidth;
         options.height = windowHeight;
+        
+        if (disableEffects) extendNofxTimelineOptions(options);
         return options;
+    }
+    
+    function updateWindowSettings() {
+        windowWidth = $(window).width();
+        windowHeight = $(window).height();
+        this.isPortrait = windowWidth < windowHeight;
+    }
+    
+    function getUrlParameters () {
+        var parametersObject = {};
+        var parameters = window.location.search.substr(1);
+        if (parameters == "") return {};
+        parameters = parameters.split('&');
+        for (var i = 0; i < parameters.length; i++) {
+            var splitParameters = parameters[i].split('=');
+            parametersObject[ splitParameters[0] ] = splitParameters[1];
+        }
+        return parametersObject;
+    };
+    
+    function extendNofxTimelineOptions(options) {
+        var effectsOptions = {
+            article: {
+                animation: {
+                    fade: {
+                        active: false
+                    },
+                    move: {
+                        active: false
+                    }
+                },
+                autostacking: {
+                    range: Histropedia.RANGE_SCREEN
+                }
+            }
+        }
+        $.extend(true, options, effectsOptions)
     }
     
     //add window resize event passing this context
