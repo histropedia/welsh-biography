4d551601bc4ccab39bf2daec8c389f9f6c1a2a28
navino <navino@histropedia.com>
2019-10-23 02:33:12 +0100

    Add search result categories
    Style groups using .autocomplete-group class
    - Added group labels to search results using
    temporary localisation until client side translations
    setup
    Note: with this library you have to manually sort the results by group

--- client/js/App.search.js
+++ client/js/App.search.js
@@ -11,29 +11,57 @@ var $searchElement;
 App.prototype.setupTimelineSearch = function(inputSelector) {
     $searchElement = $(inputSelector);
     var me = this;
+
+    // Todo: setup client side i18n
+    var groupLabels = {
+        en_GB: {
+            contextEvent: "Welsh History",
+            biographyEvent: "People"
+        },
+
+        cy: {
+            contextEvent: "Hanes Cymru",
+            biographyEvent: "Bobl"
+        },
+    }
+    
     var searchResults = this.timeline.articles.map(function(article) {
+        var type = article.data.isContextEvent ? "contextEvent" : "biographyEvent";
         return {
             value: article.data.title,
             data: {
                 id: article.id,
                 subtitle: article.data.subtitle,
-                description: article.data.description || ""
+                description: article.data.description || "",
+                type: type,
+                category: groupLabels[currentLang][type]
             }
         }
+    })
+
+    // Need to sort by category to avoid multiple instance of each group
+    searchResults.sort(function(result) {
+        return (result.data.type === "contextEvent") ? +1 : -1;
     })
 
     $searchElement.autocomplete({
         lookup: searchResults,
         lookupLimit: 100,
-        //groupBy: "category",
+        groupBy: "category",
         onSelect: function (result) {
             var articleId = result.data.id,
-                article = me.timeline.getArticleById(articleId);
-
+                article = me.timeline.getArticleById(articleId),
+                wasArticleHiddenByFilter = article.hiddenByFilter;
+            // make article visible if it's not already
+            // if hidden by filter, it will disappear again when you 
+            article.hiddenByFilter = false; 
             me.timeline.bringFront(articleId); //select and bring to front
             article.activated(); // fire click event 
             var pixelOffsetX = (me.isMobile) ? 45 : 560;
-            me.timeline.goToDateAnim(article.period.from, {offsetX: pixelOffsetX});
+            me.timeline.goToDateAnim(article.period.from, {
+                offsetX: pixelOffsetX,
+                complete: function() {article.hiddenByFilter = wasArticleHiddenByFilter; console.log("DONE!")}
+            });
         },
         formatResult: function (result) {
             return '<span class="search-result-label">' + result.value + '</span><br><span class="search-result-date">' + result.data.subtitle + '</span>';
