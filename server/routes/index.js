e0be373dbcbc9e2a04fe5c1049788282d0ccdff6
navino <navino@histropedia.com>
2019-12-15 16:04:31 +0000

    Change main debug namespace to 'dwb'
    Also update sample.env to show all dwb: debug messages

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var debug = require('debug')('server:routes');
+var debug = require('debug')('dwb:routes');
 var i18n = require('i18n');
 
 /* GET home page. */
