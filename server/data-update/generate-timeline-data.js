9c865b4a2a2ea32ac81594d8e1ed208d405c5522
navino <navino@histropedia.com>
2019-12-16 17:26:02 +0000

    Fix low precision dates
    Unify dates according to precision so all display in the same way.
    This is needed because Wikidata uses a range of year values for
    decade precision dates and below.

--- server/data-update/generate-timeline-data.js
+++ server/data-update/generate-timeline-data.js
@@ -2,7 +2,8 @@
  * Combines separate coreData and filterData query results
  * Returns timeline data in HistropediaJS format 
  */
-
+require('dotenv').config();
+var debug = require('debug')('dwb:data-update:generate-timeline-data');
 
  // Dictionary of Welsh Biography uses different domains for en and cy version
  var biographyUrlRoot = {
@@ -18,23 +19,31 @@ module.exports = function(coreData, filterData, lang) {
       id: parseInt(result.id.value),
       title: result.title.value,
       rank: parseInt(result.rank.value),
-      from: {
+      from: getPrecisionFixedDate({
         year: parseInt(result.from_year.value),
         month: parseInt(result.from_month.value),
         day: parseInt(result.from_day.value),
         precision: parseInt(result.from_precision.value),
-      },
+      }),
       dwbUrl: biographyUrlRoot[lang] + result.dwbId.value
     }
     if (result.imageUrl) nextArticle.imageUrl = result.imageUrl.value;
     if (result.article) nextArticle.article = result.article.value;
     if (result.description) nextArticle.description = result.description.value;
     if (result.to_year) {
-      nextArticle.to = {
+      nextArticle.to = getPrecisionFixedDate({
         year: parseInt(result.to_year.value),
-        month: parseInt(result.to_year.value),
+        month: parseInt(result.to_month.value),
         day: parseInt(result.to_day.value),
         precision: parseInt(result.to_precision.value),
+      })
+    } else {
+      // If no end date, copy start date so precision can be used to set time span
+      nextArticle.to = {
+        year: nextArticle.from.year,
+        month: nextArticle.from.month,
+        day: nextArticle.from.day,
+        precision: nextArticle.from.precision,
       }
     }
     return nextArticle;
@@ -65,4 +74,85 @@ function addFiltersToArticleData(articleData, filterData) {
     article.statements = articleStatements;
   }
   return articleData;
+}
+
+function getPrecisionFixedDate(date) {
+  // Wikidata dates at low precision can have inconsistent values for day, month, year
+  // This function unifies them so each precision uses the same rules
+  // See https://www.wikidata.org/wiki/Help:Dates for more info on the ranges of values covered by each precision
+  // HistropediaJS uses e.g. year=500 for 5th Century, year=2000 for 2nd millenium
+
+  // Todo: Adjust for BC dates
+  var precision = date.precision;
+  if (precision <= 9) {
+    date.day = 1;
+    date.month = 1;
+  } else if (precision === 10) {
+    date.day = 1;
+  }
+  switch (precision) {
+    case 11:
+      // Day precision
+      break;
+    case 10:
+      // Month precision
+      break;
+    case 9:
+      // Year precision
+      break;
+    case 8:
+      // Decade precision
+      date.year = Math.floor(date.year/10) * 10;
+      break;
+    case 7:
+      // Century precision
+      date.year = Math.ceil(date.year/100) * 100;
+      break;
+    case 6:
+      // Century precision
+      date.year = Math.ceil(date.year/1000) * 1000;
+      break;
+    default:
+      debug("Unknown precision: ", precision);
+  }
+
+  return date;
+}
+
+function getPrecisionFixedDate(date) {
+  // Wikidata dates at low precision can have inconsistent values for day, month, year
+  // This function unifies them so each precision uses the same rules
+  // See https://www.wikidata.org/wiki/Help:Dates for more info on the ranges of values covered by each precision
+  // Todo: Adjust for BC dates
+
+  var precision = date.precision;
+  if (precision <= 9) date.month = 1;
+  if (precision <= 10) date.day = 1;
+
+  if (precision > 8) return date;
+
+  switch (precision) {
+    case 8:
+      // Decade precision
+      date.year = Math.floor(date.year/10) * 10;
+      break;
+    case 7:
+      // Century precision
+      // e.g. 2nd century
+      // Wikidata use any year from 101 to 200
+      // HistropediaJS uses year=200
+      date.year = Math.ceil(date.year/100) * 100;
+      break;
+    case 6:
+      // Century precision
+      // e.g. 2nd millennium
+      // Wikidata use any year from 1001 to 2000
+      // HistropediaJS uses year=2000
+      date.year = Math.ceil(date.year/1000) * 1000;
+      break;
+    default:
+      debug("Unknown precision: ", precision);
+  }
+
+  return date;
 }
