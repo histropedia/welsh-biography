2c2ad81ce21a0b37600136304b03964fd1e89862
navino <navino@histropedia.com>
2020-02-25 15:55:45 +0000

    Move UI events setup to separate file
    Separate module which registers all UI events for a given App instance.

--- client/index.js
+++ client/index.js
@@ -1,4 +1,5 @@
 import {App} from './js/App';
+import {registerEvents} from './js/events';
 
 // Todo: remove once client side translation system complete
 window.LANG = $('html').attr("lang");
@@ -11,106 +12,9 @@ $.getJSON('data/' + LANG + '/timeline-data.json', function(timelineData) {
 
     // Initialise app and render timeline
     window.DWB = new App(timelineData,"#timeline-container");
-})
 
-
-// Click events
-
-$('#btn-open-color-code').click( function() {
-    DWB.openColorCodePanel();
-});
-
-$('#btn-world-events').click( function() {
-    var isActive = DWB.timeline.showContextEvents = !DWB.timeline.showContextEvents;
-    if (isActive) {
-        $(this).addClass('active');
-    } else {
-        $(this).removeClass('active');
-    }
-    DWB.filtersChanged();
-
-});
-
-$('#show-search-btn').click( function() {
-    if ( !$(this).hasClass("active") ) {
-        $('#timeline-title-container').css("display", "none");
-        $('.event-title-container').css("display", "flex");
-        $(this).addClass("active");
-    } else {
-        $('#timeline-title-container').css("display", "flex");
-        $('.event-title-container').css("display", "none");
-        $(this).removeClass("active");
-    }
-});
-
-$('#btn-close-color-code-desktop, #btn-close-color-code-mobile').click( function() {
-    DWB.closeColorCodePanel();
-});
-
-$('#color-code-select').on('change', function() { 
-    var selection = $(this).val();
-    DWB.setColorCode(selection)
-})
-
-$('#btn-open-filters').click(function() {
-    DWB.openFilterTypesPanel()
-})
-
-$('.btn-close-filter-types-panel').click(function() { //todo: temporary selector
-    DWB.closeFilterTypesPanel()
-})
-
-$('.btn-close-filter-panel').click(function() {
-    DWB.closeFilterSearchPanel();
-})
-
-$('#filter-types-list-container').on('click', 'button', function() {
-    var filterProperty = $(this).attr('filter-property');
-    DWB.openFilterSearchPanel(filterProperty);
-})
-
-$('#btn-clear-all-filters').click(function() {
-    DWB.clearAllFilters();
-})
-
-$('#btn-open-info').click(function() {
-    DWB.openInfoPanel()
-})
-
-$('.btn-close-info-panel').click(function() {
-    DWB.closeInfoPanel()
-})
-
-$('#active-filters-container, #panel-active-filters-container').on('click', 'a', function() {
-    var $tag = $(this).closest('.active-filter-tag'),
-        property = $tag.attr('filter-property'),
-        value = $tag.attr('filter-value');
-    DWB.removeFilter(property, value);
-})
-
-$('.timeline-controls-set').on("click", "a", function(ev) {
-    var buttonId = $(this).attr("id");
-    switch (buttonId) {
-        case "zoom-in-btn":
-            DWB.timeline.setZoom(DWB.timeline.timescaleManager.zoom - 1.5);
-            break;
-        case "zoom-out-btn":
-            DWB.timeline.setZoom(DWB.timeline.timescaleManager.zoom + 1.5);
-            break;
-        case "fit-screen-btn":
-            DWB.timeline.fitArticles();
-    }
-})
-
-$('#search-box').on('input js-input', function() {
-    // 'input' for normal user input, 'js-input' for script updated input
-    if ($(this).val() !== "" ) {
-        $('#btn-close-reading-panel').css('visibility','visible')
-    } else if ( !DWB.contentPanel.isOpen ) {
-        // Do not hide clear search button if content panel is open
-        // because it doubles as the close button
-        $('#btn-close-reading-panel').css('visibility','hidden')
-    }
+    // Setup clicks and other UI events
+    registerEvents(DWB);
 })
 
 function appendWidthToImageUrls(articleData,width) {
---
+++ client/js/events.js
@@ -0,0 +1,112 @@
+
+// Links UI elements with app functions via clicks and other events
+// Note: Some other modules have event handlers setup internally as well, but
+// generally this file is what needs editing if you make changes/additions to the UI
+
+export function registerEvents(app) {
+
+    // Toggle display of Welsh History context events
+    $('#btn-world-events').click(function () {
+        var isActive = app.timeline.showContextEvents = !app.timeline.showContextEvents;
+        if (isActive) {
+            $(this).addClass('active');
+        } else {
+            $(this).removeClass('active');
+        }
+        app.filtersChanged();
+
+    });
+
+    // Toggle to show/hide search box (only applicable on small screen device)
+    $('#show-search-btn').click(function () {
+        if (!$(this).hasClass("active")) {
+            $('#timeline-title-container').css("display", "none");
+            $('.event-title-container').css("display", "flex");
+            $(this).addClass("active");
+        } else {
+            $('#timeline-title-container').css("display", "flex");
+            $('.event-title-container').css("display", "none");
+            $(this).removeClass("active");
+        }
+    });
+
+    // Colour code panel
+    $('#btn-open-color-code').click(function () {
+        app.openColorCodePanel();
+    });
+
+    $('#btn-close-color-code-desktop, #btn-close-color-code-mobile').click(function () {
+        app.closeColorCodePanel();
+    });
+
+    $('#color-code-select').on('change', function () {
+        var selection = $(this).val();
+        app.setColorCode(selection)
+    })
+
+    // Filter panel
+    $('#btn-open-filters').click(function () {
+        app.openFilterTypesPanel()
+    })
+
+    $('.btn-close-filter-types-panel').click(function () { //todo: temporary selector
+        app.closeFilterTypesPanel()
+    })
+
+    $('.btn-close-filter-panel').click(function () {
+        app.closeFilterSearchPanel();
+    })
+
+    $('#filter-types-list-container').on('click', 'button', function () {
+        var filterProperty = $(this).attr('filter-property');
+        app.openFilterSearchPanel(filterProperty);
+    })
+
+    $('#btn-clear-all-filters').click(function () {
+        app.clearAllFilters();
+    })
+
+    // Info panel
+    $('#btn-open-info').click(function () {
+        app.openInfoPanel()
+    })
+
+    $('.btn-close-info-panel').click(function () {
+        app.closeInfoPanel()
+    })
+
+    // Remove filter from click on 'x' in from filter tag
+    $('#active-filters-container, #panel-active-filters-container').on('click', 'a', function () {
+        var $tag = $(this).closest('.active-filter-tag'),
+            property = $tag.attr('filter-property'),
+            value = $tag.attr('filter-value');
+        app.removeFilter(property, value);
+    })
+
+    // Floating timeline controls
+    $('.timeline-controls-set').on("click", "a", function (ev) {
+        var buttonId = $(this).attr("id");
+        switch (buttonId) {
+            case "zoom-in-btn":
+                app.timeline.setZoom(app.timeline.timescaleManager.zoom - 1.5);
+                break;
+            case "zoom-out-btn":
+                app.timeline.setZoom(app.timeline.timescaleManager.zoom + 1.5);
+                break;
+            case "fit-screen-btn":
+                app.timeline.fitArticles();
+        }
+    })
+    
+    // Hide search 'x' button when empty
+    $('#search-box').on('input js-input', function () {
+        // 'input' for normal user input, 'js-input' for script updated input
+        if ($(this).val() !== "") {
+            $('#btn-close-reading-panel').css('visibility', 'visible')
+        } else if (!app.contentPanel.isOpen) {
+            // Do not hide clear search button if content panel is open
+            // because it doubles as the close button
+            $('#btn-close-reading-panel').css('visibility', 'hidden')
+        }
+    })
+}
